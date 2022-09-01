7c0ef4c4ab27d992ec22afa5dd0c8984aa98cfea
IamSolaSholzy01 <ogolagunju@gmail.com>
2022-09-01 12:21:21 +0100

    Initial commits

--- main.cpp
+++ main.cpp
@@ -5,11 +5,20 @@ int max(int a, int b) {
     // This is a helper function
     // Input: Two integers to be compared
     // Output: The highest response from the inputs
-    return (a < b) ? a : b;
+    return (a > b) ? a : b;
+}
+
+int maximum(int *numbers, int length) {
+    int max_num = 0;
+    for (int i = 0; i < length; i++) {
+        max_num = max(numbers[i], max_num);
+    }
+    return max_num;
 }
 
 int main() {
+    int numbers[4] = {1, 2, 3, 5};
     cout << "Hello, World! Welcome to this take on the popular scrabble game." << endl;
-    cout << max(14, (int)(pow(4, 2) + 1e-9));
+    cout << maximum(numbers, sizeof(numbers) / sizeof(int));
     return 0;
 }
