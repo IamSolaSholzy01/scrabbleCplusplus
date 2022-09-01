77175e65405ff8696720a3602fae4c3acf93cc15
IamSolaSholzy01 <ogolagunju@gmail.com>
2022-09-01 11:18:08 +0100

    Initial commits

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <cmath>
+using namespace std;
+int max(int a, int b) {
+    // This is a helper function
+    // Input: Two integers to be compared
+    // Output: The highest response from the inputs
+    return (a < b) ? a : b;
+}
 
 int main() {
-    std::cout << "Hello, World! Welcome to this take on the popular scrabble game." << std::endl;
+    cout << "Hello, World! Welcome to this take on the popular scrabble game." << endl;
+    cout << max(14, (int)(pow(4, 2) + 1e-9));
     return 0;
 }
